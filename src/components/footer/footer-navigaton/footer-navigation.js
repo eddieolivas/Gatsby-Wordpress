09e64c4ace1b19327c3877b50498f05f13e1f639
Eddie Olivas <eddie@Eddies-MacBook-Pro.local>
2020-11-27 13:00:41 -0800

    Updated modal and footer link styles.

--- src/components/footer/footer-navigaton/footer-navigation.js
+++ src/components/footer/footer-navigaton/footer-navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from "gatsby";
 import { GetMenuItems } from "../../../hooks/getMenuItems";
+import footerNavStyles from './footer-navigation.module.scss';
 
 const FooterNav = () => {
     const footerNavLinks = GetMenuItems().footerNav;
     const footerNav = footerNavLinks.map((navItem, i) => (
             [
-                <Link key={i} to={navItem.path}>{navItem.label}</Link>,
+                <Link className={footerNavStyles.navItem} key={i} to={navItem.path}>{navItem.label}</Link>,
                 <span key={i+'after'}>{i !== footerNavLinks.length - 1 ? '|' : null}</span>
             ]
     ));
